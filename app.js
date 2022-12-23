3d4c162722fd725882c74872798cf13c9e2c0ce7
msozkan90 <msozkan90@gmail.com>
2022-12-23 15:43:20 +0300

    first_commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const dotenv = require("dotenv").config({ path: "config.env" });
+const methodOverride = require("method-override");
+const connectDB = require("./server/database/database");
+const routes = require("./server/routes/routes")
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set ejs
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(expressLayouts);
+
+// Middleware
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+
+// Connect db
+connectDB();
+
+// router
+app.use(routes);
+
+app.listen(port, function () {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
+
+
+
+
+
+
+// npm init -y
+// npm install bootstrap@4.1
+// npm install nodemon
+// npm install body-parser
+// npm install mongoos
+// npm install dotenv
+// npm install express
+// npm install ejs
